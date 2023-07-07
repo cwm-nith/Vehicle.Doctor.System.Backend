dea002b8a6101a64fe460eb2f84d15458ff67130
codewithmecoder <yaty1031@gmail.com>
2023-07-08 00:23:06 +0700

    add redis

---
+++ Vehicle.Doctor.System.API/Infrastructure/ExtensionCollectionServices.cs
@@ -0,0 +1,15 @@
+﻿using Vehicle.Doctor.System.API.Applications.Configurations;
+
+namespace Vehicle.Doctor.System.API.Infrastructure;
+
+public static class ExtensionCollectionServices
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ApplicationSetting appSetting)
+    {
+        services.AddStackExchangeRedisCache(otp =>
+        {
+            otp.Configuration = appSetting.Redis.Url;
+        });
+        return services;
+    }
+}
--- Vehicle.Doctor.System.API/Program.cs
+++ Vehicle.Doctor.System.API/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.Extensions.Configuration;
+using Vehicle.Doctor.System.API.Applications.Configurations;
+using Vehicle.Doctor.System.API.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var appSetting = new ApplicationSetting();
+builder.Configuration.GetSection("AppSetting").Bind(appSetting);
+builder.Services.AddSingleton(appSetting);
+
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
