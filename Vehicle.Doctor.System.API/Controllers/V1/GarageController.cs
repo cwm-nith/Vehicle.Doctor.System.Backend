7b018ec410c0eb273864eeae60240104a8b15723
codewithmecoder <yaty1031@gmail.com>
2023-07-08 23:13:21 +0700

    init garage

---
+++ Vehicle.Doctor.System.API/Controllers/V1/GarageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NN.POS.System.API.Controllers;
+
+namespace Vehicle.Doctor.System.API.Controllers.V1;
+
+[ApiVersion("1")]
+public class GarageController : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult> GetAsync()
+    {
+        var a = await Task.FromResult("Hello");
+        return Ok(a);
+    }
+}
