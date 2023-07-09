9f9f8e65077afe0c92bf510e267f881fa1485bc1
codewithmecoder <yaty1031@gmail.com>
2023-07-09 08:35:56 +0700

    garage > create database tables

---
+++ Vehicle.Doctor.System.API/Infrastructure/Tables/Configurations/UserTableConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vehicle.Doctor.System.API.Infrastructure.Tables.Garages;
+using Vehicle.Doctor.System.API.Infrastructure.Tables.Users;
+
+namespace Vehicle.Doctor.System.API.Infrastructure.Tables.Configurations;
+
+public static class UserTableConfiguration
+{
+    public static ModelBuilder AddUserTableRelationship(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserTable>()
+            .HasMany<GarageTable>()
+            .WithOne(i => i.User)
+            .HasForeignKey(i=> i.UserId);
+        return modelBuilder;
+    }
+}
---
+++ Vehicle.Doctor.System.API/Infrastructure/Tables/Garages/GarageTable.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Vehicle.Doctor.System.API.Infrastructure.Tables.BaseTables;
+using Vehicle.Doctor.System.API.Infrastructure.Tables.Users;
+
+namespace Vehicle.Doctor.System.API.Infrastructure.Tables.Garages;
+
+[Table("garages")]
+public class GarageTable : BaseTable, ISoftDeleteTable, IAuditableTable
+{
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [Column("address")] 
+    public string? Address { get; set; }
+
+    [Column("lat")]
+    public double Lat { get; set; }
+
+    [Column("long")]
+    public double Long { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; } = string.Empty;
+
+    [Column("user_id")] 
+    public long UserId { get; set; }
+
+    [Column("deleted_at")]
+    public DateTime? DeletedAt { get; set; }
+
+    [Column("deleted_by")]
+    public long? DeletedBy { get; set; }
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [Column("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [Column("updated_by")]
+    public long? UpdatedBy { get; set; }
+
+    public UserTable? User { get; set; }
+
+    public List<GarageContactTable>? GarageContacts { get; set; }
+}
