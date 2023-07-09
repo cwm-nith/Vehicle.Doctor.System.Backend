e3a5056d95f34fd5906f52918fdb3207cc93949d
codewithmecoder <yaty1031@gmail.com>
2023-07-09 08:56:24 +0700

    garage > init repo

---
+++ Vehicle.Doctor.System.API/Applications/IRepositories/Garages/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Vehicle.Doctor.System.API.Applications.Repositories.Garages;
+
+namespace Vehicle.Doctor.System.API.Applications.IRepositories.Garages;
+
+public static class Extensions
+{
+    public static void AddGarageRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<IGarageRepository, GarageRepository>();
+        services.AddTransient<IGarageContactRepository, GarageContactRepository>();
+        services.AddTransient<IGarageSocialLinkRepository, GarageSocialLinkRepository>();
+    }
+}
