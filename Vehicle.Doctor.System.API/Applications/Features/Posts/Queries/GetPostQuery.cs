c0cc345c144c93d3a46e59d65b9eab8c1f245dc3
codewithmecoder <yaty1031@gmail.com>
2023-07-11 01:31:57 +0700

    post > get posts

---
+++ Vehicle.Doctor.System.API/Applications/Features/Posts/Queries/GetPostQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Vehicle.Doctor.System.Common.Pagination;
+using Vehicle.Doctor.System.Shared.Dto.Posts;
+
+namespace Vehicle.Doctor.System.API.Applications.Features.Posts.Queries;
+
+public class GetPostQuery : IRequest<PagedResult<PostDto>>
+{
+    public PagedQuery Q { get; set; }
+
+    public GetPostQuery(PagedQuery q)
+    {
+        Q = q;
+    }
+}
--- Vehicle.Doctor.System.API/Applications/Repositories/Posts/PostRepository.cs
+++ Vehicle.Doctor.System.API/Applications/Repositories/Posts/PostRepository.cs
@@ -1,17 +1,29 @@
-﻿using Vehicle.Doctor.System.API.Applications.Entities.Posts;
+﻿using Microsoft.EntityFrameworkCore;
+using Vehicle.Doctor.System.API.Applications.Entities.Posts;
 using Vehicle.Doctor.System.API.Applications.IRepositories.Posts;
 using Vehicle.Doctor.System.API.Infrastructure.Tables;
 using Vehicle.Doctor.System.API.Infrastructure.Tables.Posts;
+using Vehicle.Doctor.System.Common.Pagination;
 
 namespace Vehicle.Doctor.System.API.Applications.Repositories.Posts;
 
 public class PostRepository : IPostRepository
 {
     private readonly IWriteDbRepository<PostTable> _writeDbRepository;
+    private readonly IReadDbRepository<PostTable> _readDbRepository;
 
-    public PostRepository(IWriteDbRepository<PostTable> writeDbRepository)
+    public PostRepository(IWriteDbRepository<PostTable> writeDbRepository, IReadDbRepository<PostTable> readDbRepository)
     {
         _writeDbRepository = writeDbRepository;
+        _readDbRepository = readDbRepository;
+    }
+
+    public async Task<PagedResult<PostEntity>> GetAsync(PagedQuery q, CancellationToken cancellationToken = default)
+    {
+        var context = _readDbRepository.Context;
+        var posts = await context.Posts!.Include(i => i.Comments)
+            .Include(i => i.Likes).PaginateAsync(q, cancellationToken);
+        return posts.Map(i => i.ToEntity());
     }
 
     public async Task<PostEntity> CreateAsync(PostEntity postEntity, CancellationToken cancellationToken = default)
