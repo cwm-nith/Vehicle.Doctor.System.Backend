2c5cb4d56c347ccd8f3e90e107f1c466bf6471bc
codewithmecoder <yaty1031@gmail.com>
2023-07-11 01:22:42 +0700

    post > create

---
+++ Vehicle.Doctor.System.API/Applications/Entities/Posts/PostEntity.cs
@@ -0,0 +1,25 @@
+﻿using Vehicle.Doctor.System.Shared.Enums.Posts;
+
+namespace Vehicle.Doctor.System.API.Applications.Entities.Posts;
+
+
+public class PostEntity : IEntity
+{
+    public long Id { get; set; }
+    public string? Description { get; set; }
+    public PostEnums.PrivacyType PrivacyType { get; set; }
+    public string? Urls { get; set; }
+    public long PosterId { get; set; }
+    public long GarageId { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public long? DeletedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public long? UpdatedBy { get; set; }
+
+    public string? NumberOfLikes => Likes?.Count.ToString();
+    public string? NumberOfComments => Comments?.Count.ToString();
+
+    public List<LikeEntity>? Likes { get; set; }
+    public List<CommentEntity>? Comments { get; set; }
+}
--- Vehicle.Doctor.System.API/Controllers/V1/Posts/PostController.cs
+++ Vehicle.Doctor.System.API/Controllers/V1/Posts/PostController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Vehicle.Doctor.System.API.Applications.Constants;
+using Vehicle.Doctor.System.API.Applications.Features.Posts.Commands;
+using Vehicle.Doctor.System.Shared.Dto.Posts;
 
 namespace Vehicle.Doctor.System.API.Controllers.V1.Posts;
 
 [ApiVersion(ApiVersionConstant.V1)]
 public class PostController : BaseApiController
 {
+    private readonly IMediator _mediator;
+
+    public PostController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     [HttpGet]
     public ActionResult GetAsync()
     {
         return Ok();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PostDto>> CreateAsync([FromBody] CreatePostDto r)
+    {
+        var cmd = new CreatePostCommand(UserId, r);
+        var data = await _mediator.Send(cmd);
+        return Ok(data);
     }
 }
