4ad990d1fe98f11b35050637773d54dc5ff37023
codewithmecoder <yaty1031@gmail.com>
2023-07-12 00:04:42 +0700

    post > like > get count

--- Vehicle.Doctor.System.API/Applications/IRepositories/Posts/ILikeRepository.cs
+++ Vehicle.Doctor.System.API/Applications/IRepositories/Posts/ILikeRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Vehicle.Doctor.System.API.Applications.IRepositories.Posts;
+﻿using System.Linq.Expressions;
+using Vehicle.Doctor.System.API.Applications.Entities.Posts;
+using Vehicle.Doctor.System.API.Infrastructure.Tables.Posts;
+
+namespace Vehicle.Doctor.System.API.Applications.IRepositories.Posts;
 
 public interface ILikeRepository
 {
-    
+    Task<int> GetCountAsync(Expression<Func<LikeTable, bool>> predicate, CancellationToken cancellation = default);
+    Task<bool> DeleteAsync(Expression<Func<LikeTable, bool>> predicate, CancellationToken cancellation = default);
+    Task<LikeEntity> CreateAsync(LikeEntity entity, CancellationToken cancellation = default);
 }
--- Vehicle.Doctor.System.API/Applications/Repositories/Posts/LikeRepository.cs
+++ Vehicle.Doctor.System.API/Applications/Repositories/Posts/LikeRepository.cs
@@ -1,8 +1,37 @@
-﻿using Vehicle.Doctor.System.API.Applications.IRepositories.Posts;
+﻿using System.Linq.Expressions;
+using Vehicle.Doctor.System.API.Applications.Entities.Posts;
+using Vehicle.Doctor.System.API.Applications.IRepositories.Posts;
+using Vehicle.Doctor.System.API.Infrastructure.Tables;
+using Vehicle.Doctor.System.API.Infrastructure.Tables.Posts;
+using Vehicle.Doctor.System.Shared.Dto.Likes;
 
 namespace Vehicle.Doctor.System.API.Applications.Repositories.Posts;
 
 public class LikeRepository : ILikeRepository
 {
-    
+    private readonly IWriteDbRepository<LikeTable> _writeDbRepository;
+    private readonly IReadDbRepository<LikeTable> _readDbRepository;
+
+    public LikeRepository(IWriteDbRepository<LikeTable> writeDbRepository, IReadDbRepository<LikeTable> readDbRepository)
+    {
+        _writeDbRepository = writeDbRepository;
+        _readDbRepository = readDbRepository;
+    }
+
+    public Task<int> GetCountAsync(Expression<Func<LikeTable, bool>> predicate, CancellationToken cancellation = default)
+    {
+        return _readDbRepository.CountAsync(predicate, cancellation);
+    }
+
+    public async Task<bool> DeleteAsync(Expression<Func<LikeTable, bool>> predicate, CancellationToken cancellation = default)
+    {
+        var num = await _writeDbRepository.DeleteAsync(predicate, cancellation);
+        return num > 0;
+    }
+
+    public async Task<LikeEntity> CreateAsync(LikeEntity entity, CancellationToken cancellation = default)
+    {
+        var data = await _writeDbRepository.AddAsync(entity.ToTable(), cancellation);
+        return data.ToEntity();
+    }
 }
